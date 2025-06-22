fe3a185800594d4f2773683d754508f0cee2ff51
Kamlesh Lovewanshi <85796224+python-monster08@users.noreply.github.com>
2025-06-22 19:39:34 +0000

    Add Algorithms in comments

--- DSA Learning/DSA_Kamlesh/two_pointers_problems/TwoSum.py
+++ DSA Learning/DSA_Kamlesh/two_pointers_problems/TwoSum.py
@@ -1,3 +1,27 @@
+"""
+📌 Problem:
+Given a sorted array and a target sum, find if there exists a pair with the given sum.
+
+Example:
+Input: arr = [1, 2, 3, 4, 6], target = 6
+Output: True (because 2+4 = 6)
+
+# **************************************** Traditional (Brute Force) Approach *************************************************
+✅ Idea: Check all possible pairs using nested loops.
+
+📖 Algorithm:
+1. Run two nested loops.
+
+2. For each element arr[i], check every other element arr[j].
+
+3. If arr[i] + arr[j] == target, return True.
+
+4. If no pair found, return False.
+
+📦 Time Complexity: O(n²)
+📦 Space Complexity: O(1)
+"""
+
 print("######################## Brute Force ################################")
 
 def has_pair_brute_force(arr, target):
@@ -17,6 +41,26 @@ print(has_pair_brute_force(arr, target))
 
 print()
 
+
+# ************************************* Optimized Approach using Two Pointers ********************************************
+"""
+✅  Idea: Since the array is sorted, we can use two pointers — one starting from the beginning and one from the end.
+    If array is not sorted then apply arr.sort() function then apply two pointer.
+
+📖 Algorithm:
+1. Initialize two pointers: left = 0 and right = n-1
+
+2. While left < right:
+        (I) Calculate curr_sum = arr[left] + arr[right]
+        (II) If curr_sum == target, return True.
+        (III) If curr_sum < target, move left pointer rightward.
+        (IV) If curr_sum > target, move right pointer leftward.
+
+3. If no pair found, return False.
+
+📦 Time Complexity: O(n)
+📦 Space Complexity: O(1)
+"""
 print("######################## Two Pointers ################################")
 
 def has_pair_two_pointers(arr, target):
@@ -43,11 +87,28 @@ print(has_pair_two_pointers(arr, target))
 
 print()
 
+# ************************************ Optimized Algorithm (HashMap-based Two Sum) ******************************************
+"""
+✅  Idea: Since the array is sorted, we can use two pointers — one starting from the beginning and one from the end.
+    If array is not sorted then apply arr.sort() function then apply two pointer.
 
-print("######################## Hash Set ################################")
+📖 Algorithm:
+1. Use a HashMap (dictionary) to store the numbers you’ve seen with their indices.
+
+2. For each number, 
+    (I)Find complement target - num 
+    (II) Check if its complement (target - num) exists in the map.
+        (A) If found, return the indices.
+    (III) If not, add the number with its index to the map.
+3. If no pair found, return False.
+
+📦 Time Complexity: O(n)
+📦 Space Complexity: O(n)
+"""
+print("######################## Hash Map ################################")
 
 def has_pair_hashmap(arr, target):
-    hashmap = {}
+    hashmap = {}   # create a map
     for i,num in enumerate(arr):
         complement = target-num
         if complement in hashmap:
